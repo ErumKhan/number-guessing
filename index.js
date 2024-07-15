1771d7f97b921f6327f4536b5420d9f2d8d7ad10
ErumKhan <ErumKhan@users.noreply.github.com>
2024-07-15 15:26:33 +0500

    Creating numberb guessing Game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//const randomNumber =3; // hard codded
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const number = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Please Guess the number from 1 t0 10: ",
+    },
+]);
+if (number.UserGuessedNumber === randomNumber) {
+    console.log("Conngratulations! You guessed the right number.");
+}
+else {
+    console.log("Oooppps! Your guessed is wrong number");
+}
